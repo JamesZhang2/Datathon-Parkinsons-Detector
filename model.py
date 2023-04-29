5226cbc43dc09c3d2738cc7132ac780498db0906
JamesZhang2 <jameszhang2019@outlook.com>
2023-04-29 15:30:39 -0400

    Implement CNN model
    Co-Authored-By: srishagaur <97795859+srishagaur@users.noreply.github.com>
    Co-Authored-By: lisarli <62680027+lisarli@users.noreply.github.com>

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+# import torch.nn.functional as F
 from sklearn.model_selection import train_test_split
 from torchvision import transforms
 from torch.utils.data import TensorDataset
@@ -53,26 +54,59 @@ train_circle_labels = torch.tensor(
     [0] * len(train_healthy_imgs) + [1] * len(train_patient_imgs)
 )
 
-dataset = TensorDataset(train_circle_imgs, train_circle_labels)
-# print(type(dataset))
+test_circle_imgs = torch.cat((test_healthy_imgs, test_patient_imgs), dim=0)
+test_circle_labels = torch.tensor(
+    [0] * len(test_healthy_imgs) + [1] * len(train_patient_imgs)
+)
 
-dataloader = torch.utils.data.DataLoader(dataset, shuffle=True)
+train_set = TensorDataset(train_circle_imgs, train_circle_labels)
+train_loader = torch.utils.data.DataLoader(train_set, shuffle=True)
 
 
 # Define the CNN architecture
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(3, 32, 3)  # in_channel, out_channel, ker_size
         self.conv2 = nn.Conv2d(32, 64, 3)
-        self.fc1 = nn.Linear(64 * 6 * 6, 128)
+        self.conv3 = nn.Conv2d(64, 32, 3)
+        self.pool = nn.MaxPool2d(2, 2)  # ker_size, stride
+        self.fc1 = nn.Linear(21632, 128)
         self.fc2 = nn.Linear(128, 2)
 
     def forward(self, x):
         x = self.pool(nn.functional.relu(self.conv1(x)))
         x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = self.pool(nn.functional.relu(self.conv3(x)))
         x = torch.flatten(x, 1)
+        # print(x.shape)
         x = nn.functional.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+
+cnn = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(cnn.parameters(), lr=0.001)
+
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        # Clear the gradients
+        optimizer.zero_grad()
+
+        # Pass the batch of images through the CNN
+        outputs = cnn(images)
+
+        # Compute the loss
+        loss = criterion(outputs, labels)
+
+        # Compute the gradients
+        loss.backward()
+
+        # Update the weights
+        optimizer.step()
+
+    # Print some information about the training progress
+    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}")
